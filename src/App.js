3a3515f78bbde09ef5cdf78bc8b48a6e4e335e2e
EDPRIME-SYSTEM-\Edprime System 2 <imsanjay390@gmail.com>
2022-01-28 21:42:50 +0530

    e comm

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Cart from "./containers/Cart";
+import ProductList from "./containers/ProductList";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row"></div>
+      <div className="row">
+        <div className="col-md-8">
+          <ProductList />
+        </div>
+        <div className="col-md-4">
+          <Cart />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
